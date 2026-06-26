d900ca754856b43f7312de2b835425447d467571
levelp <onojasteven13@gmail.com>
2026-06-26 12:24:37 +0100

    Changes

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,5 +1,5 @@
 #################################################
-# ECS TASK EXECUTION ROLE (pull image, logs, secrets)
+# ECS TASK EXECUTION ROLE (AWS INTERNAL USE)
 #################################################
 
 resource "aws_iam_role" "ecs_task_execution_role" {
@@ -7,34 +7,73 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
-
-  lifecycle {
-    create_before_destroy = true
-  }
 
   tags = {
     Name = var.ecs_task_execution_role_name
   }
 }
 
-#################################################
-# ATTACH AWS MANAGED EXECUTION POLICY
-#################################################
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+resource "aws_iam_role_policy_attachment" "execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+#################################################
+# ECS TASK ROLE (YOUR APP INSIDE CONTAINER)
+#################################################
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = var.ecs_task_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = var.ecs_task_role_name
+  }
+}
+
+#################################################
+# ECS EXEC (CONNECT INTO CONTAINER) PERMISSIONS
+#################################################
+
+resource "aws_iam_policy" "ecs_exec_policy" {
+  name = "ecs-exec-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel",
+          "ecs:ExecuteCommand"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 
 #################################################
 # OPTIONAL: SECRETS MANAGER ACCESS (EXECUTION ROLE)
@@ -64,30 +103,11 @@ resource "aws_iam_role_policy_attachment" "ecs_secrets_attach" {
 }
 
 #################################################
-# ECS TASK ROLE (YOUR APPLICATION PERMISSIONS)
+# ATTACH ECS EXEC POLICY TO TASK ROLE
+# (optional but useful for debugging)
 #################################################
 
-resource "aws_iam_role" "ecs_task_role" {
-  name = var.ecs_task_role_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name = var.ecs_task_role_name
-  }
+resource "aws_iam_role_policy_attachment" "ecs_exec_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_exec_policy.arn
 }
